40d66d537c43fe207ea87ba590af8441e0a72fac
Brett Nelson <Brett.Nelson@Magnet360.com>
2016-10-05 15:54:35 -0700

    Init

---
+++ 00-block-scoped-variables/es6.js
@@ -0,0 +1,13 @@
+
+var publicMessage = 'message';
+
+{
+    var somethingIDontWanToShare = `secret ${publicMessage}`;
+
+    console.log('Inside the "block scope"');
+    console.log(somethingIDontWanToShare);
+}
+
+console.log('Outside the block scope');
+console.log(publicMessage);
+console.log(somethingIDontWanToShare);
